dc8d7d91b2b7cbdc99fc322a68dfd354843efb1d
Wanli Zhou <pamzwl@gmail.com>
2024-07-17 02:46:27 +0800

    fixed the contract initiation code

--- contract_factory/ignition/modules/arbitrage.js
+++ contract_factory/ignition/modules/arbitrage.js
@@ -1,8 +1,7 @@
 const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 
 module.exports = buildModule("arbitrage", (m) => {
-  const arbitrage = m.contract("Arbitrage", {
-  });
+  const arbitrage = m.contract("Arbitrage");
 
   return { arbitrage };
 });
