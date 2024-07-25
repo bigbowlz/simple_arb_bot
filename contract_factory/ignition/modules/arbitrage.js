3f450fdbea49fd8b078faa52c137afe714bc336d
Wanli Zhou <pamzwl@gmail.com>
2024-07-25 22:36:01 +0800

    Stop tracking contract_factory directory

--- contract_factory/ignition/modules/arbitrage.js
+++
@@ -1,7 +0,0 @@
-const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
-
-module.exports = buildModule("arbitrage", (m) => {
-  const arbitrage = m.contract("Arbitrage");
-
-  return { arbitrage };
-});
