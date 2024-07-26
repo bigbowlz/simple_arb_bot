b2297cd3a45bf85e356df0ae5885e0e4594dd28b
Wanli Zhou <pamzwl@gmail.com>
2024-07-26 21:05:16 +0800

    fix setup shell script except for route

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,3 +1,5 @@
+# Run the script using: chmod +x scripts/setup.sh && ./scripts/setup.sh
+
 # Check if port 8545 is in use and kill the process if found
 PORT=8545
 PID=$(lsof -t -i:$PORT)
@@ -22,12 +24,12 @@ if [ $? -eq 0 ]; then
     abi="python utilities/populate_routes.py --ABI" # Generate ABI files for on-chain router contracts (Uniswap, PancakeSwap, and SushiSwap)
     route="python utilities/populate_routes.py --route" # Iterate through tokens and routers in the config file to find valid routes
     compile="npx hardhat compile" # Compile contracts 
-    deploy="npx hardhat ignition deploy ./ignition/modules/arbitrage.js --network localhost; npx hardhat ignition deploy ./ignition/modules/BTC.js --network localhost; npx hardhat ignition deploy ./ignition/modules/USDC.js --network localhost;" #Deploy the arbitrage contract and test tokens USDC iCAN and BTC iCAN
+    deploy="npx hardhat ignition deploy ./ignition/modules/arbitrage.js --network localhost && npx hardhat ignition deploy ./ignition/modules/BTC.js --network localhost && npx hardhat ignition deploy ./ignition/modules/USDC.js --network localhost" #Deploy the arbitrage contract and test tokens USDC iCAN and BTC iCAN
     pool="npx hardhat run scripts/approveTokenAndLP.js" # Approve USDC iCAN and BTC iCAN for routers and create a liquidity pool
 
     # Open a new terminal window and run the commands
     # osascript -e "tell application \"Terminal\" to do script \"cd ${current_dir} && ${abi} && ${route} && ${compile} && ${deploy} && ${pool}\""
-    osascript -e "tell application \"Terminal\" to do script \"cd ${current_dir} && ${abi} && ${route}\""
+    osascript -e "tell application \"Terminal\" to do script \"cd ${current_dir} && ${compile} && ${deploy} && ${abi}\""
 
 else
     echo "Node initialization failed for the Ethereum fork."
