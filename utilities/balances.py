98ec106a0a6d2884988c1b107235f3c8aa84581a
Wanli Zhou <pamzwl@gmail.com>
2024-08-05 03:03:27 +0800

    add get_account_balances(

--- utilities/balances.py
+++ utilities/balances.py
@@ -10,6 +10,8 @@ Version: 08-04-2024
 '''
 from decimal import Decimal
 from web3 import Web3
+import os
+import json
 
 def to_wei(amount, decimals):
     """
@@ -66,14 +68,237 @@ def get_token_decimals(token_address, web3):
 
     token_contract = web3.eth.contract(address=token_address, abi=erc20_abi)
     return token_contract.functions.decimals().call()
-'''from populate_routes import (setup)
 
-def get_ERC20_balance(accountAddress, assetAddress):
-    
-    Gets and logs the balance of an asset in an account. 
-    
-    return {} #{assetAddress: balance}
-
-def get_ETH_balance(accountAddress, assetAddress):
-'''
-
+def get_account_balances(arb_bot):
+#     erc20_abi = '''
+#     [
+# {
+# "constant": true,
+# "inputs": [],
+# "name": "name",
+# "outputs": [
+# {
+# "name": "",
+# "type": "string"
+# }
+# ],
+# "payable": false,
+# "stateMutability": "view",
+# "type": "function"
+# },
+# {
+# "constant": false,
+# "inputs": [
+# {
+# "name": "_spender",
+# "type": "address"
+# },
+# {
+# "name": "_value",
+# "type": "uint256"
+# }
+# ],
+# "name": "approve",
+# "outputs": [
+# {
+# "name": "",
+# "type": "bool"
+# }
+# ],
+# "payable": false,
+# "stateMutability": "nonpayable",
+# "type": "function"
+# },
+# {
+# "constant": true,
+# "inputs": [],
+# "name": "totalSupply",
+# "outputs": [
+# {
+# "name": "",
+# "type": "uint256"
+# }
+# ],
+# "payable": false,
+# "stateMutability": "view",
+# "type": "function"
+# },
+# {
+# "constant": false,
+# "inputs": [
+# {
+# "name": "_from",
+# "type": "address"
+# },
+# {
+# "name": "_to",
+# "type": "address"
+# },
+# {
+# "name": "_value",
+# "type": "uint256"
+# }
+# ],
+# "name": "transferFrom",
+# "outputs": [
+# {
+# "name": "",
+# "type": "bool"
+# }
+# ],
+# "payable": false,
+# "stateMutability": "nonpayable",
+# "type": "function"
+# },
+# {
+# "constant": true,
+# "inputs": [],
+# "name": "decimals",
+# "outputs": [
+# {
+# "name": "",
+# "type": "uint8"
+# }
+# ],
+# "payable": false,
+# "stateMutability": "view",
+# "type": "function"
+# },
+# {
+# "constant": true,
+# "inputs": [
+# {
+# "name": "_owner",
+# "type": "address"
+# }
+# ],
+# "name": "balanceOf",
+# "outputs": [
+# {
+# "name": "balance",
+# "type": "uint256"
+# }
+# ],
+# "payable": false,
+# "stateMutability": "view",
+# "type": "function"
+# },
+# {
+# "constant": true,
+# "inputs": [],
+# "name": "symbol",
+# "outputs": [
+# {
+# "name": "",
+# "type": "string"
+# }
+# ],
+# "payable": false,
+# "stateMutability": "view",
+# "type": "function"
+# },
+# {
+# "constant": false,
+# "inputs": [
+# {
+# "name": "_to",
+# "type": "address"
+# },
+# {
+# "name": "_value",
+# "type": "uint256"
+# }
+# ],
+# "name": "transfer",
+# "outputs": [
+# {
+# "name": "",
+# "type": "bool"
+# }
+# ],
+# "payable": false,
+# "stateMutability": "nonpayable",
+# "type": "function"
+# },
+# {
+# "constant": true,
+# "inputs": [
+# {
+# "name": "_owner",
+# "type": "address"
+# },
+# {
+# "name": "_spender",
+# "type": "address"
+# }
+# ],
+# "name": "allowance",
+# "outputs": [
+# {
+# "name": "",
+# "type": "uint256"
+# }
+# ],
+# "payable": false,
+# "stateMutability": "view",
+# "type": "function"
+# },
+# {
+# "payable": true,
+# "stateMutability": "payable",
+# "type": "fallback"
+# },
+# {
+# "anonymous": false,
+# "inputs": [
+# {
+# "indexed": true,
+# "name": "owner",
+# "type": "address"
+# },
+# {
+# "indexed": true,
+# "name": "spender",
+# "type": "address"
+# },
+# {
+# "indexed": false,
+# "name": "value",
+# "type": "uint256"
+# }
+# ],
+# "name": "Approval",
+# "type": "event"
+# },
+# {
+# "anonymous": false,
+# "inputs": [
+# {
+# "indexed": true,
+# "name": "from",
+# "type": "address"
+# },
+# {
+# "indexed": true,
+# "name": "to",
+# "type": "address"
+# },
+# {
+# "indexed": false,
+# "name": "value",
+# "type": "uint256"
+# }
+# ],
+# "name": "Transfer",
+# "type": "event"
+# }
+# ]
+#     '''
+#     weth_abi = '''[{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"guy","type":"address"},{"name":"wad","type":"uint256"}],"name":"approve","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"src","type":"address"},{"name":"dst","type":"address"},{"name":"wad","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"wad","type":"uint256"}],"name":"withdraw","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"balanceOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"dst","type":"address"},{"name":"wad","type":"uint256"}],"name":"transfer","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"deposit","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"},{"name":"","type":"address"}],"name":"allowance","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"payable":true,"stateMutability":"payable","type":"fallback"},{"anonymous":false,"inputs":[{"indexed":true,"name":"src","type":"address"},{"indexed":true,"name":"guy","type":"address"},{"indexed":false,"name":"wad","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"src","type":"address"},{"indexed":true,"name":"dst","type":"address"},{"indexed":false,"name":"wad","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"dst","type":"address"},{"indexed":false,"name":"wad","type":"uint256"}],"name":"Deposit","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"src","type":"address"},{"indexed":false,"name":"wad","type":"uint256"}],"name":"Withdrawal","type":"event"}]'''
+    with open("configs/mainnet.json", "r") as file:
+        data = json.load(file)
+    print("Data read from json file.")
+    print(f'ETH - {arb_bot.web3.eth.get_balance(arb_bot.bot_address)}')
+    for token in data["tokens"]:
+        token_address = token["address"]
+        print(f'{token["sym"]} - {arb_bot.get_balance(token_address)}')
